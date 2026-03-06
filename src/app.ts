a31b2e3edb56658eb1e30fca640fcdb28e5d8329
ShelDenis <shele1-2010@mail.ru>
2026-03-06 17:38:49 +0600

    init

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'nativescript-vue';
+import Home from './components/Home.vue';
+import Menu from './components/Menu.vue'
+
+// createApp(Home).start();
+createApp(Menu).start();
---
+++ src/components/Menu.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import {
+  ref,
+  computed,
+  onMounted,
+  onUnmounted,
+  $navigateTo,
+} from 'nativescript-vue';
+import Details from './Details.vue';
+import { Label, ScrollView } from '@nativescript/core';
+</script>
+
+<template>
+  <Frame>
+    <Page actionBarHidden="true">
+        <StackLayout>
+
+          <Label text="QueLab" class="quelab-title" />
+          <Button text="Профиль" @tap="onProfile" class="vibe-button" />
+          <Button text="Активные очереди" @tap="onActiveQueues" class="vibe-button" />
+          <Button text="Создать очередь" @tap="onCreateQueue" class="vibe-button" />
+
+        </StackLayout>
+    </Page>
+  </Frame>
+</template>
+
+<style>
+/* .info {
+    font-size: 20;
+  } */
+</style>
